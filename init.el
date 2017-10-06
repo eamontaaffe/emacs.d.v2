dd82b7912b5f3645baca0dcec1ae678711be3e03
Eamon Taaffe <eamontaaffe@gmail.com>
2017-10-06 11:26:40 +1100

    added site-list load

--- init.el
+++ init.el
@@ -5,11 +5,20 @@
 (setq inhibit-startup-message t)
 
 ;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
 (setq settings-dir
       (expand-file-name "settings" user-emacs-directory))
 
 ;; Set up load path
 (add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
 
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
