c9b592560493ae1e97fa21f1b270daa24e493ace
Eamon Taaffe <eamontaaaffe@gmail.com>
2017-10-11 21:32:10 +1100

    Updated variables

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (multiple-cursors graphql-mode undo-tree alchemist flycheck-credo flycheck-flow exec-path-from-shell flycheck web-mode helm-projectile projectile helm-ls-git helm smex magit better-defaults)))
+    (helm-ag multiple-cursors graphql-mode undo-tree alchemist flycheck-credo flycheck-flow exec-path-from-shell flycheck web-mode helm-projectile projectile helm-ls-git helm smex magit better-defaults)))
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
